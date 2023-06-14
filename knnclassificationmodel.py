e130c9e0a0db11f758f5b1024edde0d52222b13b
dharmakolla <dharmakolla85@gmail.com>
2023-06-14 09:56:17 +0530

    Update knnclassificationmodel.py

--- knnclassificationmodel.py
+++ knnclassificationmodel.py
@@ -25,11 +25,19 @@ categorical_columns = ['ID','DEVICETYPE', 'BIDREQUESTIP','USERCITY', 'USERPLATFO
 # Create an instance of LabelEncoder
 label_encoder = LabelEncoder()
 
+dictionary_taxonomy = {}
+
 # Encode categorical variables in the selected columns
 for col in categorical_columns:
+    value = train_data['TAXONOMY']
+    if col == 'TAXONOMY':
+        #print(train_data['TAXONOMY'].size)
+        for index in range(value.size):
+            #if train_data['TAXONOMY'][index] != 'NaN':
+            dictionary_taxonomy.setdefault(value[index],index)
     train_data[col] = label_encoder.fit_transform(train_data[col])
 
-#print(train_data.describe())
+#print(dictionary_taxonomy)
 
 # Extract the input features and target variable
 X_train_hcp = train_data.drop('IS_HCP', axis=1)  # Assuming 'IS_HCP' is the column name for the target variable
@@ -76,45 +84,68 @@ y_test_taxonomy = test_data['TAXONOMY']
 
 #Accuracy for the KNeighborsClassifier algorithm 
 # is 0.8931070678796361 with neighbors 4
-max_accuracy = 0
-index = 0
-for i in range(1000):
-    model_hcp = KNeighborsClassifier(n_neighbors=i+1)
-    model_taxonomy = KNeighborsClassifier(n_neighbors=i+1)
+max_accuracy_hcp = 0
+max_accuracy_taxonomy = 0
+index_hcp = 0
+index_taxonomy = 0
+#for i in range(1000):
+model_hcp = KNeighborsClassifier(n_neighbors=1000)
+model_taxonomy = KNeighborsClassifier(n_neighbors=1000)
 
-    X_test_hcp = X_test_hcp.fillna(-1)
-    y_test_hcp = y_test_hcp.fillna(-1)
+X_test_hcp = X_test_hcp.fillna(-1)
+y_test_hcp = y_test_hcp.fillna(-1)
 
-    X_test_taxonomy = X_test_taxonomy.fillna(-1)
-    y_test_taxonomy = y_test_taxonomy.fillna(-1)
+X_test_taxonomy = X_test_taxonomy.fillna(-1)
+y_test_taxonomy = y_test_taxonomy.fillna(-1)
 
 
-    X_train_hcp = X_train_hcp.fillna(-1)
-    y_train_hcp = y_train_hcp.fillna(-1)
+X_train_hcp = X_train_hcp.fillna(-1)
+y_train_hcp = y_train_hcp.fillna(-1)
 
-    X_train_taxonomy = X_train_taxonomy.fillna(-1)
-    y_train_taxonomy = y_train_taxonomy.fillna(-1)
+X_train_taxonomy = X_train_taxonomy.fillna(-1)
+y_train_taxonomy = y_train_taxonomy.fillna(-1)
 
 # Train the model on the training data
-    model_hcp.fit(X_train_hcp, y_train_hcp)
+model_hcp.fit(X_train_hcp, y_train_hcp)
 
 # Make predictions on the test data
-    y_pred_hcp = model_hcp.predict(X_test_hcp)
+y_pred_hcp = model_hcp.predict(X_test_hcp)
 
 # Calculate the accuracy of the model
-    accuracy = accuracy_score(y_test_hcp, y_pred_hcp)
-    print(f'Accuracy: {accuracy}')
+max_accuracy_hcp = accuracy_score(y_test_hcp, y_pred_hcp)
+    #print(f'Accuracy: {accuracy}')
 
-    if(accuracy >= max_accuracy):
-        max_accuracy = accuracy
-        index = i
+# Train the model on the training data
+model_taxonomy.fit(X_train_taxonomy, y_train_taxonomy)
 
-    result = test_data
-    result = result.filter(['ID','TAXONOMY','IS_HCP'])
+# Make predictions on the test data
+y_pred_taxonomy = model_taxonomy.predict(X_test_taxonomy)
+
+# Calculate the accuracy of the model
+max_accuracy_taxonomy = accuracy_score(y_test_taxonomy, y_pred_taxonomy)
+    #print(f'Accuracy: {accuracy}')
 
 
-    result['IS_HCP'] = y_pred_hcp
-    result = result.filter(['ID','TAXONOMY','IS_HCP'])
-print(f'Max Accuracy: {max_accuracy}')
-print(f'Index: {index}')
+
+    # if(accuracy_hcp >= max_accuracy_hcp):
+    #     max_accuracy_hcp = accuracy_hcp
+    #     index_hcp = i
+    #     if( max_accuracy_hcp == 1.0):
+    #         break
+
+    # if(accuracy_taxonomy >= max_accuracy_taxonomy):
+    #     max_accuracy_taxonomy = accuracy_taxonomy
+    #     index_taxonomy = i
+    #     if( max_accuracy_taxonomy == 1.0):
+    #         break
+
+result = test_data
+result['TAXONOMY'] = y_pred_taxonomy
+result = result.filter(['ID','TAXONOMY','IS_HCP'])
+
+
+result['IS_HCP'] = y_pred_hcp
+result = result.filter(['ID','TAXONOMY','IS_HCP'])
+print(f'Max Accuracy for hcp: {max_accuracy_hcp}')
+print(f'Max Accuracy for taxonomy: {max_accuracy_taxonomy}')
 result.to_csv('output.csv')
